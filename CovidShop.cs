d2f0bbde5a180a9119a0544580a3e1b34deb3ffd
Løven <43264940+mark-knudsen@users.noreply.github.com>
2021-01-20 19:44:45 +0100

    Add files via upload

---
+++ CovidShop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidCounter
+{
+    public class CovidShop
+    {
+        public int Id { get; set; }
+        public string Butiksnavn { get; set; }
+        public int Antal { get; set; }
+        public int AntalMax {get; set;}
+
+
+        public CovidShop (int id, String butiksnavn, int antal, int antalMax)
+        {
+            Id = id;
+            Butiksnavn = butiksnavn;
+            Antal = antal;
+            AntalMax = antalMax;
+
+        }
+    }
+}
